c99d38ef30037fb9e2e5a08766c3e83b1303adc5
Brian Soe <bsoe@ucsd.edu>
2016-10-01 21:02:35 -0700

    changed script back to native

--- public/es6/script.js
+++ public/es6/script.js
@@ -1,26 +1,24 @@
+(function () {
 
-(function() {
-    "use strict";
-
-    /* Initialize Variables */
-    var ScreenSize = {
+    /* Initialize Constants */
+    const ScreenSize = {
         TABLET: 992,
         MOBILE: 600
     };
-    var ButtonColor = {
+    const ButtonColor = {
         NORMAL: '#424242',
         SUCCESS: '#4CAF50',
         FAILED: '#F44336'
     };
-    var pathname = window.location.pathname.slice(1);
-    var isEntity = pathname.match(new RegExp('^p/'));
-    var RequestAnimationFrame = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
+    const pathname = window.location.pathname.slice(1);
+    const isEntity = pathname.match(new RegExp('^p/'));
+    const RequestAnimationFrame = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
 
-    var LazyLoad = function() {
+    const LazyLoad = () => {
         $('body').hide(); // in case page shows up before css is fully loaded
 
-        $('noscript').each(function() {
-            var element = $(this).text();
+        $('noscript').each(() => {
+            const element = $(this).text();
             $(element).insertBefore($(this));
             $(this).remove();
         });
@@ -28,25 +26,28 @@
         $('head > style').remove();
 
         // delay for smoother loading of body
-        setTimeout(function() {
+        setTimeout(() => {
             $('body').fadeIn('500');
         }, 0);
-    }
+    };
 
-    var CloseModal = function() {
+    const CloseModal = () => {
         $('._modal_').hide();
         $('body').css({ 'overflow': 'visible' });
     };
 
-    var LoadLeaflet = function() {
+    const LoadLeaflet = () => {
+        let mymap;
         L.Icon.Default.imagePath = '/media/leaflet';
-        var mymap = L.map('map', { scrollWheelZoom: false }).setView([32.8818006, -117.2357122], 12);
+        mymap = L.map('map', { scrollWheelZoom: false }).setView([32.8818006, -117.2357122], 12);
         L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnNvZSIsImEiOiJjaXJoZmN5emcwMWFjZzNreGhzOXhnMGVuIn0.hx7spsB6PS8BNYvAsWVcmA', {
             accessToken: 'pk.eyJ1IjoiYnNvZSIsImEiOiJjaXJoZmN5emcwMWFjZzNreGhzOXhnMGVuIn0.hx7spsB6PS8BNYvAsWVcmA'
         }).addTo(mymap);
         L.marker([32.8818006, -117.2357122]).addTo(mymap);
-        mymap.once('focus', function() { mymap.scrollWheelZoom.enable(); });
-        mymap.on('click', function() {
+        mymap.once('focus', () => {
+            mymap.scrollWheelZoom.enable();
+        });
+        mymap.on('click', () => {
             if (mymap.scrollWheelZoom.enabled()) {
                 mymap.scrollWheelZoom.disable();
             } else {
@@ -55,7 +56,7 @@
         });
     };
 
-    var Initialize = function() {
+    const Initialize = () => {
         // LazyLoad(); // lazy load CSS file
 
         // setup navbar
@@ -80,17 +81,16 @@
         }
 
         // delay google analytics tracking
-        setTimeout(function() {
-            (function(i, s, o, g, r, a, m) {
+        setTimeout(() => {
+            ((i, s, o, g, r, a, m) => {
                 i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function() {
-                        (i[r].q = i[r].q || []).push(arguments)
-                    }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                    m = s.getElementsByTagName(o)[0];
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments);
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o), m = s.getElementsByTagName(o)[0];
                 a.async = 1;
                 a.src = g;
-                m.parentNode.insertBefore(a, m)
+                m.parentNode.insertBefore(a, m);
             })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
             ga('create', 'UA-82807303-1', 'auto');
@@ -98,11 +98,11 @@
         }, 0);
 
         console.log("Hello developer(s)! Here are some quick links you might be interested in:\n * Resume: http://briansoe.com/resume\n * Repository: https://github.com/bsoe003/website");
-    }
+    };
 
     /* Start Initialization */
     if (RequestAnimationFrame) {
-        RequestAnimationFrame(function() {
+        RequestAnimationFrame(() => {
             setTimeout(Initialize, 0);
         });
     } else {
@@ -110,42 +110,48 @@
     }
 
     // transition out right before exit
-    window.addEventListener("beforeunload", function() {
+    window.addEventListener("beforeunload", () => {
         document.body.classList.add("animate-out");
     });
 
     // exit any modal upon pushing "esc"
-    $(document).keyup(function(e) {
+    $(document).keyup(e => {
         if (e.keyCode == 27) {
             CloseModal();
         }
     });
 
-    // Javascript alternative to open on new window
-    $('a').click(function(e) {
+    // Javascript alternatives for anchor tags
+    $('a').click(e => {
+        const href = $(this).attr('href');
+        // opens in new window (without relying on _blank)
         if ($(this).attr('target') === '_blank') {
-            window.open($(this).attr('href'));
+            window.open(href);
+            return false;
+        }
+        // prevents request if user is currently in that page
+        if (href === window.location.pathname) {
             return false;
         }
     });
 
     // click anywhere but nav dropdown
-    $('body').click(function(e) {
-        var width = $('body').width();
+    $('body').click(e => {
+        const width = $('body').width();
         if (width <= ScreenSize.MOBILE && !$(e.target).is('nav > ul > li > a')) {
             $('nav > ul').slideUp();
         }
     });
 
     // nav dropdown event handler
-    $('#mobile-menu').click(function(e) {
+    $('#mobile-menu').click(e => {
         e.stopPropagation();
         $('nav > ul').slideToggle();
     });
 
     // nav dropdown screen handler
-    $(window).resize(function() {
-        var width = $('body').width();
+    $(window).resize(() => {
+        const width = $('body').width();
         if (width > ScreenSize.MOBILE) {
             $('nav > ul').show();
         } else {
@@ -154,30 +160,31 @@
     });
 
     // close modal when clicking anywhere but modal
-    $(window).click(function(e) {
+    $(window).click(e => {
         if ($(e.target).is($('._modal_'))) {
             CloseModal();
         }
     });
 
     // open modal; disable scrolling for background;
-    $('#open-contact-form').click(function(e) {
+    $('#open-contact-form').click(e => {
         $('#contact-form-box').show();
         $('body').css({ 'overflow': 'hidden' });
     });
 
     // close modal
-    $('._close_').click(function(e) {
+    $('._close_').click(e => {
         CloseModal();
     });
 
     // contact form handler
-    $('#contact-form').submit(function(e) {
+    $('#contact-form').submit(e => {
         e.preventDefault();
-        var name = e.target.name.value.trim();
-        var email = e.target.email.value.trim();
-        var subject = e.target.subject.value.trim();
-        var content = e.target.content.value.trim();
+
+        const name = e.target.name.value.trim();
+        const email = e.target.email.value.trim();
+        const subject = e.target.subject.value.trim();
+        const content = e.target.content.value.trim();
         if (!email || !subject || !content) {
             e.target.submit.value = 'Error :(';
             $(e.target.submit).css({ 'background-color': ButtonColor.FAILED });
@@ -195,10 +202,10 @@
             contentType: 'application/json',
             data: JSON.stringify({ name: name, _email: email, subject: subject, content: content }),
             dataType: 'json',
-            success: function(data) {
+            success: data => {
                 e.target.submit.value = 'Success :)';
                 $(e.target.submit).css({ 'background-color': ButtonColor.SUCCESS });
-                setTimeout(function() {
+                setTimeout(() => {
                     e.target.submit.value = 'Send Message';
                     $(e.target.submit).css({ 'background-color': ButtonColor.NORMAL });
                     e.target.name.value = '';
@@ -209,7 +216,7 @@
                     $('body').css({ 'overflow': 'visible' });
                 }, 3000);
             },
-            error: function() {
+            error: () => {
                 e.target.submit.value = 'Error :(';
                 $(e.target.submit).css({ 'background-color': ButtonColor.FAILED });
             },
@@ -218,5 +225,4 @@
             url: '/mail'
         });
     });
-
 }).call(this);
